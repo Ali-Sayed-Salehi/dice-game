43bb282582efd8508b77cd6f40e256d49045c0d6
Ali Sayed Salehi <a.s.salehi.95@gmail.com>
2023-05-10 17:43:40 -0400

    add title message logic

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 
 
 randomNumber1 = Math.floor((Math.random() * 6)) + 1;
+randomNumber2 = Math.floor((Math.random() * 6)) + 1;
 
 const dice1img = document.querySelector('.img1');
-dice1img.setAttribute("src", `images/dice${randomNumber1}.png`);
+const dice2img = document.querySelector('.img2');
 
-console.log(dice1img);
+dice1img.setAttribute("src", `images/dice${randomNumber1}.png`);
+dice2img.setAttribute("src", `images/dice${randomNumber2}.png`);
+
+const titleMessage= document.querySelector('h1');
+
+if (randomNumber1 > randomNumber2) {
+  titleMessage.textContent = "Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+  titleMessage.textContent = "Player 2 Wins!";
+} else {
+  titleMessage.textContent = "Draw!";
+}
+
+
